ce413ab96511226a82689f8edea2c41db93aa179
jesus <jesus@gmail.com>
2019-02-03 16:42:13 -0600

    cambios

--- RailsPryectos/ecommerce/app/controllers/application_controller.rb
+++ RailsPryectos/ecommerce/app/controllers/application_controller.rb
@@ -25,11 +25,9 @@ class ApplicationController < ActionController::Base
   	def set_shopping_cart
 			if cookies[:shopping_cart_id].blank?
 				@shopping_cart = ShoppingCart.create!(ip: request.remote_ip)
-
 				cookies[:shopping_cart_id] = @shopping_cart.id
 			else
 				@shopping_cart = ShoppingCart.find(cookies[:shopping_cart_id])
-
 			end
 
 			rescue ActiveRecord::RecordNotFound => e
