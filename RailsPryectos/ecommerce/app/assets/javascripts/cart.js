fb55c81603ef2f21557c3799c71f6539bdba4558
Jesus <jesus.alberto.vk@gmail.com>
2019-02-07 21:01:44 -0600

    cambios

--- RailsPryectos/ecommerce/app/assets/javascripts/cart.js
+++ RailsPryectos/ecommerce/app/assets/javascripts/cart.js
@@ -1,26 +1,13 @@
-var info = {
-  nombre: "jesus",
-  appe: "sadasdas",
-  p: null,
-  q: null,
-  user: {}
-}
-var metodos = {
-  showAll(){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => {
-      return response.json()
-    })
-    .then(json => {
-      info.user = json
-      info.p.innerText = info.user.id
-      info.q[0].innerText = info.user.title
-      info.q[1].innerText = info.user.userId
-    })
-  }
-}
-$(document).ready(function(){
-  info.p = document.getElementById('ejemplo')
-  info.q = document.getElementsByClassName('ejemplo')
-  metodos.showAll()
+$(document).ready(function () {
+  var p = document.getElementById("ejemplo")
+  p.innerText = 0
+  p.addEventListener("click", function () {
+    enviarTexto()
+  })
 })
+
+function enviarTexto() {
+  var p = document.getElementById("ejemplo")
+  var temporal_n = parseInt(p.innerText)
+  p.innerText = temporal_n + 1
+}
--- RailsPryectos/ecommerce/test/fixtures/categories.yml
+++ RailsPryectos/ecommerce/test/fixtures/categories.yml
@@ -3,12 +3,12 @@
 # Table name: categories
 #
 #  id                 :integer          not null, primary key
-#  name               :string
-#  description        :string
-#  c_img_file_name    :string
 #  c_img_content_type :string
+#  c_img_file_name    :string
 #  c_img_file_size    :integer
 #  c_img_updated_at   :datetime
+#  description        :string
+#  name               :string
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #
